bd3a562beb67e88f84ca5b17a93651161e467085
Mateus Prado <maateussp@gmail.com>
2026-01-09 12:26:43 -0300

    build: validators

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
+    whitelist: true,  //Remove chaves que não estao no DTO
+    forbidNonWhitelisted: true, //Levantar erro quando a chave nao existir
+    transform: true // Tenta transformar os tipos de dados de parametros e dtos 
    }));
   await app.listen(process.env.PORT ?? 3000);
  
