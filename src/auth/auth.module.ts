c1877d262b4fd4532f033dbbd58a2d1062e8613a
Mateus Prado <maateussp@gmail.com>
2026-02-02 11:48:08 -0300

    Gerando um token se os dados estiverem corretos

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,31 @@
 import { Global, Module } from "@nestjs/common";
 import { HashingServiceProtocol } from "./hashing/hashing.service";
 import { BcrypyService } from "./hashing/bcrypt.service";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Person } from "src/persons/entities/person.entity";
+import {JwtModule} from '@nestjs/jwt'
+import { ConfigModule } from "@nestjs/config";
+import jwtConfig from "./config/jwt.config";
 
 @Global()
 @Module({
+    imports: [TypeOrmModule.forFeature([Person]),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ],
+    controllers:
+        [AuthController],
     providers: [
         {
             provide: HashingServiceProtocol,
             useClass: BcrypyService,
         },
+        AuthService
     ],
     exports: [HashingServiceProtocol]
+
 })
 export class AuthModule {
 
